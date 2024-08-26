46121fc6a234124d5b74b3d255627973500d234f
Dima Tisnek <dima.tisnek@canonical.com>
2024-08-26 14:46:03 +0900

    cleanup🧼

--- test/test_main_invocations.py
+++ test/test_main_invocations.py
@@ -1,24 +1,22 @@
 from typing import reveal_type
 
-from ops import CharmBase
-
 
 def test_top_level_import():
     import ops
 
     reveal_type(ops.main)
-    ops.main(CharmBase)
+    assert ops.main() == 42
 
 
 def test_submodule_import():
     import ops.main
 
     reveal_type(ops.main)
-    ops.main(CharmBase)
+    assert ops.main() == 42
 
 
 def test_function_import():
     from ops import main
 
     reveal_type(main)
-    main(CharmBase)
+    assert main() == 42
